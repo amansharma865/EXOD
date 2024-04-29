dc21b1e5c4124084e93ebe769cfb6a20fdba64b0
amansharma865 <158766559+amansharma865@users.noreply.github.com>
2024-04-29 12:26:03 +0530

    Delete EXOD directory

--- EXOD/script.js
+++
@@ -1,7 +0,0 @@
-// 'use strict';
-
-// const preload = document.querySelector(".preload");
-// window.addEventListener("load", function(){
-//     NavigationPreloadManager.classList.add('loaded');
-//     document.body.classList.add("loaded");
-// });
