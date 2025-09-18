7e80d98da6f3e233bac1baca2e243ce3c7cacd78
Aman Sharma <amansharma865865@gmail.com>
2025-09-18 18:03:37 +0530

    revert delete

---
+++ EXOD/script.js
@@ -0,0 +1,7 @@
+// 'use strict';
+
+// const preload = document.querySelector(".preload");
+// window.addEventListener("load", function(){
+//     NavigationPreloadManager.classList.add('loaded');
+//     document.body.classList.add("loaded");
+// });
